e6f35975d9b03ea462baf52e6a28fb75438e38e4
Carlos Orue <carlos@orue.io>
2025-04-05 19:26:05 -0500

    add which-key

---
+++ lua/config/plugins/which-key.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "modern",
+    delay = 300,
+    icons = {
+      rules = false,
+      breadcrumb = " ", -- symbol used in the command line area that shows your active key combo
+      separator = "󱦰  ", -- symbol used between a key and it's label
+      group = "󰹍 ", -- symbol prepended to a group
+    },
+    plugins = {
+      spelling = {
+        enabled = false,
+      },
+    },
+
+  },
+}
