a6930bf516684412c56e75a441d65b2f2357c8e5
Carlos Orue <carlos@orue.io>
2025-06-22 22:15:30 -0500

    file formating

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,3 @@
-
 vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,23 +1,23 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,   -- Ensure it loads before other plugins
-        lazy = false,      -- Load immediately, not lazily
-        config = function()
-            require("catppuccin").setup({
-                flavour = "frappe", -- Set the flavour here: latte, frappe, macchiato, mocha
-                transparent_background = true, -- Set to true if you want transparency
-                integrations = {
-                    cmp = true,
-                    gitsigns = true,
-                    nvimtree = true,
-                    treesitter = true,
-                    telescope = true,
-                    -- Add more integrations as needed
-                },
-            })
-            vim.cmd.colorscheme "catppuccin"
-        end,
-    },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,   -- Ensure it loads before other plugins
+    lazy = false,      -- Load immediately, not lazily
+    config = function()
+      require("catppuccin").setup({
+	flavour = "frappe", -- Set the flavour here: latte, frappe, macchiato, mocha
+	transparent_background = true, -- Set to true if you want transparency
+	integrations = {
+	  cmp = true,
+	  gitsigns = true,
+	  nvimtree = true,
+	  treesitter = true,
+	  telescope = true,
+	  -- Add more integrations as needed
+	},
+      })
+      vim.cmd.colorscheme "catppuccin"
+    end,
+  },
 }
