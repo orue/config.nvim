bad47fb054cfaef3fe0fddc2d8dcb40ee826e326
Carlos Orue <carlos@orue.io>
2025-06-23 09:56:38 -0500

    Add more options and keymaps file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,13 +1,14 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
 local opt = vim.opt
 
 -- Line number
 opt.number = true
 opt.relativenumber = true
 
-
-opt.shiftwidth = 4
+opt.shiftwidth = 2
+opt.wrap = false
 opt.clipboard = "unnamedplus"
-
 
 opt.cursorline = true
 opt.termguicolors = true
@@ -15,21 +16,13 @@ opt.background = "dark"
 opt.signcolumn = "yes"
 opt.scrolloff = 8
 
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
 
 
 
-
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
-vim.keymap.set("v", "<space>x", ":lua<CR>")
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-	vim.highlight.on_yank()
-    end,
-})
