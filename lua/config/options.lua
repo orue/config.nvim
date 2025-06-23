b3be818f127ed4d2fdf4cbc2e94446639756ba7c
Carlos Orue <carlos@orue.io>
2025-06-23 09:20:17 -0500

    Add more options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,14 +1,19 @@
-vim.opt.shiftwidth = 4
-vim.opt.clipboard = "unnamedplus"
-vim.opt.number = true
-vim.opt.relativenumber = true
+local opt = vim.opt
+
+-- Line number
+opt.number = true
+opt.relativenumber = true
 
 
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
+opt.shiftwidth = 4
+opt.clipboard = "unnamedplus"
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+
+opt.cursorline = true
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+opt.scrolloff = 8
 
 
 
@@ -22,9 +27,9 @@ vim.keymap.set("v", "<space>x", ":lua<CR>")
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+	vim.highlight.on_yank()
+    end,
 })
