85413b373398893136beda0ca6af5fe02ef4f54b
Carlos Orue <carlos@orue.io>
2025-06-23 19:06:44 -0500

    setup format on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,6 +14,23 @@ return {
     },
     config = function()
       require("lspconfig").lua_ls.setup {}
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
+
+          if vim.bo.filetype == "lua" then -- check file type for format on autosave
+            -- Format the current buffer on save
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end,
+            })
+          end
+        end,
+      })
     end,
   }
 }
