dac163090e63f9df96e17690a107d5913ceb339b
Carlos Orue <carlos@orue.io>
2025-06-25 08:58:30 -0500

    install telescope

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,20 +2,20 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000,   -- Ensure it loads before other plugins
-    lazy = false,      -- Load immediately, not lazily
+    priority = 1000, -- Ensure it loads before other plugins
+    lazy = false,    -- Load immediately, not lazily
     config = function()
       require("catppuccin").setup({
-	flavour = "frappe", -- Set the flavour here: latte, frappe, macchiato, mocha
-	transparent_background = true, -- Set to true if you want transparency
-	integrations = {
-	  cmp = true,
-	  gitsigns = true,
-	  nvimtree = true,
-	  treesitter = true,
-	  telescope = true,
-	  -- Add more integrations as needed
-	},
+        flavour = "frappe",            -- Set the flavour here: latte, frappe, macchiato, mocha
+        transparent_background = true, -- Set to true if you want transparency
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          telescope = true,
+          -- Add more integrations as needed
+        },
       })
       vim.cmd.colorscheme "catppuccin"
     end,
