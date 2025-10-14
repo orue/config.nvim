f0fa017e0551cc48ee75a371e8d33f8d70069d39
Carlos Orue <carlos@orue.io>
2025-10-14 01:17:37 -0500

    add ruff and pyright

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,14 +15,44 @@ return {
     config = function()
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       require("lspconfig").lua_ls.setup { capabilities = capabilities }
+      require("lspconfig").pyright.setup { 
+        capabilities = capabilities,
+        settings = {
+          pyright = {
+            disableOrganizeImports = true,
+          },
+          python = {
+            analysis = {
+              ignore = { '*' },
+            },
+          },
+        },
+      }
+      require("lspconfig").ruff.setup { 
+        capabilities = capabilities,
+        init_options = {
+          settings = {
+            args = {},
+          }
+        },
+        on_attach = function(client, bufnr)
+          client.server_capabilities.hoverProvider = false
+        end,
+      }
 
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
           if not client then return end
 
-          if vim.bo.filetype == "lua" then -- check file type for format on autosave
-            -- Format the current buffer on save
+          if vim.bo.filetype == "lua" then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end,
+            })
+          elseif vim.bo.filetype == "python" and client.name == "ruff" then
             vim.api.nvim_create_autocmd('BufWritePre', {
               buffer = args.buf,
               callback = function()
