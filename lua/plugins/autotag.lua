e7ef9a5af4ae8d4b67cc5d3f9dc3e1d4a271784e
Carlos Orue <carlos@orue.dev>
2026-04-12 19:02:10 -0500

    Migrate nvim-ts-autotag to new setup options layout
    Nest options under `autotag` key to match the new API and silence
    the deprecation warning about legacy setup opts.

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -3,14 +3,16 @@ return {
   event = "InsertEnter",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   opts = {
-    enable_close = true,
-    enable_rename = true,
-    enable_close_on_slash = false,
-    per_filetype = {
-      ["html"] = { enable_close = true },
-      ["vue"] = { enable_close = true },
-      ["javascriptreact"] = { enable_close = true },
-      ["typescriptreact"] = { enable_close = true },
+    autotag = {
+      enable_close = true,
+      enable_rename = true,
+      enable_close_on_slash = false,
+      per_filetype = {
+        ["html"] = { enable_close = true },
+        ["vue"] = { enable_close = true },
+        ["javascriptreact"] = { enable_close = true },
+        ["typescriptreact"] = { enable_close = true },
+      },
     },
   }
 }
